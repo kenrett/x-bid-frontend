46ee9f788b37797edbad32aa26806c5c615d1fd3
Ken Rettberg <kenrettberg@gmail.com>
2025-11-16 12:02:53 -0800

    Optimizes Flowbite initialization

---
+++ src/components/FlowbiteInitializer.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+import { initFlowbite } from 'flowbite'
+
+export function FlowbiteInitializer() {
+  useEffect(() => {
+    initFlowbite()
+  }, [])
+
+  return null
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AuthProvider } from './auth/AuthProvider'
-import { initFlowbite } from 'flowbite'
+import { FlowbiteInitializer } from './components/FlowbiteInitializer'
 
-initFlowbite()
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
 
-createRoot(document.getElementById('root')!).render(
+createRoot(rootElement).render(
   <StrictMode>
     <AuthProvider>
+      <FlowbiteInitializer />
       <App />
     </AuthProvider>
   </StrictMode>,
-)
+);
